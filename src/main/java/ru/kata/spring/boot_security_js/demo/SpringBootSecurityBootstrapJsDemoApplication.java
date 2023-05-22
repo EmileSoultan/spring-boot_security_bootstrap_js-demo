a95110913ed5bd0deab452948fd6a1596eb96495
Emile <emabish42@gmail.com>
2023-05-22 12:58:38 +0200

    Initial commit

---
+++ src/main/java/ru/kata/spring/boot_security_js/demo/SpringBootSecurityBootstrapJsDemoApplication.java
@@ -0,0 +1,13 @@
+package ru.kata.spring.boot_security_js.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityBootstrapJsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityBootstrapJsDemoApplication.class, args);
+	}
+
+}
