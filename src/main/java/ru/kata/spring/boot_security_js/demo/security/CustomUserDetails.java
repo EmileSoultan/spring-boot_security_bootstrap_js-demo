bab6464f25d4e72d87f1a37efba8c203d7d06687
Emile <emabish42@gmail.com>
2023-05-27 14:05:24 +0200

    Реализация интерфейса UserDetails

---
+++ src/main/java/ru/kata/spring/boot_security_js/demo/security/CustomUserDetails.java
@@ -0,0 +1,67 @@
+package ru.kata.spring.boot_security_js.demo.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import ru.kata.spring.boot_security_js.demo.model.Role;
+import ru.kata.spring.boot_security_js.demo.model.User;
+
+import java.io.Serial;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+public class CustomUserDetails implements UserDetails {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    private final User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<Role> roles = user.getRoles();
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        for (Role role : roles) {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        }
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public String getFullName() {
+        return user.getName() + " " + user.getSurname();
+    }
+}
