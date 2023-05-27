c321c70a15fad6bf5d47def799ed47a006b41b55
Emile <emabish42@gmail.com>
2023-05-27 14:09:07 +0200

    Контроллер для передачи представления Rest api(Json) для обычного польз-ля

---
+++ src/main/java/ru/kata/spring/boot_security_js/demo/controller/UserRestController.java
@@ -0,0 +1,28 @@
+package ru.kata.spring.boot_security_js.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.kata.spring.boot_security_js.demo.model.User;
+import ru.kata.spring.boot_security_js.demo.service.UserService;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserRestController {
+    private final UserService userService;
+
+    public UserRestController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<User> getUser(Principal principal) {
+        User user = userService.findByEmail(principal.getName()).orElseThrow();
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+}
